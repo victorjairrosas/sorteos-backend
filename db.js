931f410026d67c0041f884884d53bb1e117c6e9e
victorjairrosas <victorjairrosas@gmail.com>
2026-09-10 17:34:37 -0600

    Update db.js

--- db.js
+++ db.js
@@ -8,8 +8,8 @@
 const fs = require('fs');
 const path = require('path');
 
-const COMPRAS_PATH = path.join(__dirname, 'data', 'compras.json');
-const RIFA_PATH = path.join(__dirname, 'data', 'rifa-config.json');
+   const COMPRAS_PATH = path.join(__dirname, 'compras.json');
+   const RIFA_PATH = path.join(__dirname, 'rifa-config.json');
 
 function getRifaConfig() {
   return JSON.parse(fs.readFileSync(RIFA_PATH, 'utf-8'));
